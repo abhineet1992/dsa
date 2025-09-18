aa98617e959e73195f88b235077fc16a42310c05
Abhineet Sinha <ASinha14@slb.com>
2025-09-18 15:10:20 +0530

    dsa project

--- move_zero.py
+++ move_zero.py
@@ -3,15 +3,24 @@ class Solution:
     def move_zeroes(self, nums):
         for i in range(len(nums) -1):
             if nums[i] == 0:
+                print(f"Original array is {nums}")
+                print(f"0 occurs at {i}")
                 temp = nums[i]
                 n = (len(nums) - i)
                 k = len(nums[i+1:])
-                print(f'0 occurs at {i}, \n\
-                        array after 0 is {nums[i+1:]}, \n\
-                        number of array items to be moved will be {len(nums[i+1:])}, \n\
-                        array to be overwritten {nums[i:i+k]} \n\
-                    ')
-                # print(nums[i:])
+                print(f"Array needs to be moved {nums[i+1:]}")
+                print(f"Array to be overwritten {nums[i:i+k]}")
+                nums[i:i+k] = nums[i+1:]
+                nums[-1] = temp
+                print(f"Rotated array is {nums}")
 
 solution = Solution()
-solution.move_zeroes([0,1,0,3,12])
+# solution.move_zeroes([0,1,0,3,12])
+# solution.move_zeroes([0,0,0])
+solution.move_zeroes([0,2,0,4,0,5])
+# solution.move_zeroes([1,2,3,4,5])
+# solution.move_zeroes([])
+# solution.move_zeroes([0])
+# solution.move_zeroes([0,1])
+# solution.move_zeroes([1,0])
+# solution.move_zeroes([0,0,1,0])
